0146e3fcb3fd65e9b1f621087eeb691ecd8103a9
kuritaeiji <eiji199825@yahoo.co.jp>
2020-06-21 23:55:25 +0900

    検索フォームの追加

---
+++ src/views/BookSearch.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="book-search">
+    <b-form inline v-on:submit="onSubmit">
+      <label for="keyword" class="mr-3">キーワード</label>
+      <b-input id="keyword" v-model="keyword" class="mr-3"></b-input>
+      <b-button type="submit" variant="primary">検索</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'book-search',
+  data() {
+    return {
+      books: [],
+      keyword: ''
+    }
+  }
+}
+</script>
