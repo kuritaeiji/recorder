640141da7aaa03177c753a1cfac6de04b989687d
kuritaeiji <eiji199825@yahoo.co.jp>
2020-06-22 05:50:13 +0900

    レビュー一覧

---
+++ src/views/Books.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="books">
+    <BookInfo v-for="book in books" :key="book.id" :book="book" :linkable="true"></BookInfo>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import BookInfo from '../components/BookInfo.vue'
+
+export default {
+  name: 'books',
+  data() {
+    return {
+      books: []
+    }
+  },
+  components: {
+    BookInfo
+  },
+  computed: mapGetters(['getBooks']),
+  created() {
+    this.books = this.getBooks
+  }
+}
+</script>
